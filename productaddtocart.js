23545df220d4c5e0d1fc06592d6188aa8d1c557b
Drea00 <128638059+Drea00@users.noreply.github.com>
2023-10-18 13:08:32 +0100

    Add files via upload

---
+++ productaddtocart.js
@@ -0,0 +1,41 @@
+     // Get references to the HTML elements
+     const plusButton = document.querySelector('.plus');
+     const group2 = document.querySelector('.roup');
+     const inbox= document.querySelector('.counting');
+     
+     // Function to hide .group-2 and show .group
+     function toggleGroups() {
+         group2.style.display = 'none';
+         inbox.style.display = 'block';
+     }
+     
+     // Add a click event listener to the .plus button
+     plusButton.addEventListener('click', toggleGroups);
+     
+     let numberDisplay = document.querySelector(".text-wrapper-2");   
+     let increaseButton = document.querySelector(".vector");
+     let decreaseButton = document.querySelector(".img");
+     
+     let number = 1; // Initialize the number to 1
+     
+     // Function to update and display the number
+     function updateNumber() {
+     numberDisplay.textContent = number;
+     }
+     
+     // Event listener for the increase button
+     increaseButton.addEventListener('click', function () {
+     number++;
+     updateNumber();
+     });
+     
+     // Event listener for the decrease button
+     decreaseButton.addEventListener('click', function () {
+     if (number > 0) {
+      number--;
+      updateNumber();
+     }
+     });
+     
+     // Initial display
+     updateNumber();
